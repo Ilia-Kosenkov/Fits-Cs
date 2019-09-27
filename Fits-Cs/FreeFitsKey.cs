63352aaa4482cca918e68401bc7a978d9f98787a
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-09-27 15:29:22 +0300

    Started implementaion of free-format keys

---
+++ Fits-Cs/FreeFitsKey.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Maybe;
+
+namespace FitsCs
+{
+    public abstract class FreeFitsKey : FitsKey
+    {
+        private protected static readonly string EmptyString = string.Intern(new string (' ', 20));
+
+        public override KeyType Type => KeyType.Free;
+        private protected FreeFitsKey(string name, string comment) : base(name, comment)
+        {
+        }
+
+        private protected bool FormatFree(Span<char> span, string value, out int charsWritten)
+        {
+            throw new NotImplementedException(SR.Method_Not_Implemented);
+            //var isCommentNull = string.IsNullOrWhiteSpace(Comment);
+            //charsWritten = 0;
+            //var len = NameSize +
+            //          value.Length;
+
+            //if (span.Length < EntrySizeInBytes)
+            //    return false;
+
+            //span.Slice(0, EntrySizeInBytes).Fill(' ');
+            //Name.AsSpan().CopyTo(span);
+            //value.AsSpan().CopyTo(span.Slice(NameSize));
+
+
+            //if (!isCommentNull)
+            //{
+            //    Comment.AsSpan().CopyTo(span.Slice(len + 2));
+            //    span[len + 1] = '/';
+            //}
+
+            //charsWritten = EntrySizeInBytes;
+            //return true;
+        }
+
+        public static IFitsValue<T> Create<T>(string name, Maybe<T> value, string comment = null)
+        {
+            throw new NotImplementedException(SR.Method_Not_Implemented);
+
+            //ValidateType<T>();
+
+
+            //switch (value)
+            //{
+            //    case Maybe<float> fVal:
+            //        return new FixedFloatKey(name, fVal, comment) as IFitsValue<T>;
+            //    case Maybe<int> iVal:
+            //        return new FixedIntKey(name, iVal, comment) as IFitsValue<T>;
+            //    case Maybe<bool> bVal:
+            //        return new FixedBoolKey(name, bVal, comment) as IFitsValue<T>;
+            //    case Maybe<Complex> cVal:
+            //        return new FixedComplexKey(name, cVal, comment) as IFitsValue<T>;
+            //}
+            //throw new NotSupportedException();
+        }
+    }
+}
