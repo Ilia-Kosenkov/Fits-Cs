7b6a6c3bde7e451583a1ad8418fb020d3a3c6829
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-09-27 16:58:13 +0300

    Handling fixed-format strings

---
+++ Fits-Cs/Extensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Buffers;
+using System.Text;
+using TextExtensions;
+using MemoryExtensions;
+
+namespace FitsCs
+{
+    public static class Extensions
+    {    
+        public static int StringSizeWithQuoteReplacement(this ReadOnlySpan<char> s)
+        {
+            var sum = 2;
+            foreach (var item in s)
+            {
+                // Regular character 1-to-1
+                sum += 1;
+                // Single quote 2-to-1
+                if(item == '\'')
+                    sum += 1;
+                // Double quote replaced by 4 single quotes
+                if (item == '"')
+                    sum += 3;
+            }
+
+            return sum;
+        }
+
+        public static bool TryGetCompatibleString(this ReadOnlySpan<char> source, Span<char> target)
+        {
+            if (source.IsEmpty)
+                return true;
+
+            if (target.Length < source.Length + 2)
+                return false;
+
+            target[0] = '\'';
+            var srcInd = 0;
+            var targetInd = 1;
+            
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (source[i] == '\'')
+                {
+                    if (!source.Slice((srcInd, i + 1)).TryCopyTo(target.Slice(targetInd)))
+                        return false;
+                    targetInd += i - srcInd + 1;
+                    target[targetInd++] = '\'';
+                    srcInd = i + 1;
+                }
+                else if (source[i] == '"')
+                {
+                    if (!source.Slice((srcInd, i)).TryCopyTo(target.Slice(targetInd)))
+                        return false;
+                    targetInd += i - srcInd;
+                    target.Slice(targetInd, 4).Fill('\'');
+                    targetInd += 4;
+                    srcInd = i + 1;
+                }
+
+            }
+
+            if (srcInd < source.Length)
+            {
+                if (!source.Slice(srcInd).TryCopyTo(target.Slice(targetInd)))
+                    return false;
+                targetInd += source.Length - srcInd;
+            }
+
+            if (targetInd >= target.Length)
+                return false;
+
+            target[targetInd] = '\'';
+
+            return true;
+        }
+    }
+}
---
+++ Fits-Cs/FixedStringKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Maybe;
+
+namespace FitsCs
+{
+    public sealed class FixedStringKey : FixedFitsKey, IFitsValue<string>
+    {
+        internal FixedStringKey(string name, string comment) : base(name, comment)
+        {
+        }
+
+        public override object Value => RawValue.Match(x => (object)x);
+        public override bool IsEmpty => RawValue.Match(_ => true);
+        public override bool TryFormat(Span<char> span, out int charsWritten)
+        {
+            
+            
+
+            throw new NotImplementedException(SR.Method_Not_Implemented);
+        }
+        public Maybe<string> RawValue { get; }
+
+        internal FixedStringKey(string name, Maybe<string> value, string comment) : base(name, comment)
+        {
+            RawValue = value;
+        }
+    }
+}
