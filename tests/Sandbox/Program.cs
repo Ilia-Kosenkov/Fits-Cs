be6b1707fed9b584767b0189eff1c2c759545300
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-10-04 10:29:33 +0300

    Chaning tests

--- tests/Sandbox/Program.cs
+++ tests/Sandbox/Program.cs
@@ -1,28 +1,43 @@
 ﻿using FitsCs;
 using Maybe;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 using FitsCs.Keys;
 
 namespace Sandbox
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            await Test1();
             Test2();
         }
 
-     
+        private static async Task Test1()
+        {
+
+            using (var fs = new FileStream("FOCx38i0101t_c0f.fits", FileMode.Open))
+            {
+                using (var reader = new FitsReader(fs))
+                {
+                    await reader.ReadAsync();
+                }
+            }
+        }
+
+
         private static void Test2()
         {
-            var key = FitsKey.Create("NAME", "textsome''".Some(), "comment", KeyType.Free);
-            Span<char> span = new char[90];
+            //    var key = FitsKey.Create("NAME", "textsome''".Some(), "comment", KeyType.Free);
+            //    Span<char> span = new char[90];
 
-            key.TryFormat(span);
-            var result = span.Slice(0, 80).ToString();
+            //    key.TryFormat(span);
+            //    var result = span.Slice(0, 80).ToString();
 
 
-            var anotherKey = key.With(x => x.Value = (x.Value as Maybe<string>).Select(y => y.Length));
+            //    var anotherKey = key.With(x => x.Value = (x.Value as Maybe<string>).Select(y => y.Length));
 
         }
     }
