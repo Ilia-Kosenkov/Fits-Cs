9ed8f261f2558aa9ba95464cb64c88c0ee9b8bfe
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-01-06 23:10:36 +0200

    Removed duplicated code

--- Fits-Cs/Block.cs
+++ Fits-Cs/Block.cs
@@ -11,33 +11,23 @@ namespace FitsCs
     {
         public List<IFitsValue> Keys { get; }
     
-        public ImmutableArray<int> Dimensions { get; }
-        public Type DataType { get; }
-        public byte ItemSizeInBytes { get; }
-
-        public int ParamCount { get; }
-        public int GroupCount { get; }
-        public ExtensionType Type { get; }
+        public Descriptor Descriptor { get; }
 
         public abstract Span<byte> RawData { get; }
         
         public abstract void FlipEndianessIfNecessary();
 
-        public long DataCount() => GroupCount * (Dimensions.Aggregate<long, int>(1, (prod, n) => prod * n) + ParamCount);
-        public long DataSizeInBytes() => DataCount() * ItemSizeInBytes;
+        public long DataCount() => Descriptor.GetFullSize();
+            //GroupCount * (Dimensions.Aggregate<long, int>(1, (prod, n) => prod * n) + ParamCount);
+        public long DataSizeInBytes() => DataCount() * Descriptor.ItemSizeInBytes;
 
         protected internal Block(Descriptor desc)
         {
             
             if (desc.IsEmpty)
                 throw new ArgumentException(SR.InvalidArgument, nameof(desc));
+            Descriptor = desc;
             Keys = new List<IFitsValue>(desc.Nkeys);
-            Dimensions = desc.Dimensions;
-            DataType = desc.DataType;
-            ItemSizeInBytes = desc.ItemSizeInBytes;
-            ParamCount = desc.ParamCount;
-            GroupCount = desc.GroupCount;
-            Type = desc.Type;
         }
 
         public static Type ConvertBitPixToType(int bitpix)
@@ -106,60 +96,59 @@ namespace FitsCs
 
         public override void FlipEndianessIfNecessary()
         {
-            if (!_hasCorrectEndianess)
+            if (_hasCorrectEndianess) return;
+            var span = RawData;
+            var itemSizeInBytes = Descriptor.ItemSizeInBytes;
+            if (itemSizeInBytes == 2)
             {
-                var span = RawData;
-                if (ItemSizeInBytes == 2)
+                for (var i = 0; i < _data.Length; i++)
                 {
-                    for (var i = 0; i < _data.Length; i++)
-                    {
-                        var offset = 2 * i;
-                        var temp = span[offset];
-                        span[offset] = span[offset + 1];
-                        span[offset + 1] = temp;
-                    }
+                    var offset = 2 * i;
+                    var temp = span[offset];
+                    span[offset] = span[offset + 1];
+                    span[offset + 1] = temp;
                 }
-                else if (ItemSizeInBytes == 4)
-                {
-                    for (var i = 0; i < _data.Length; i++)
-                    {
-                        var offset = 4 * i;
-                        
-                        var temp = span[offset];
-                        span[offset] = span[offset + 3];
-                        span[offset + 3] = temp;
-
-                        temp = span[offset + 1];
-                        span[offset + 1] = span[offset + 2];
-                        span[offset + 2] = temp;
-                    }
-                }
-                else if (ItemSizeInBytes == 8)
-                {
-                    for (var i = 0; i < _data.Length; i++)
-                    {
-                        var offset = 8 * i;
-
-                        var temp = span[offset];
-                        span[offset] = span[offset + 7];
-                        span[offset + 7] = temp;
-
-                        temp = span[offset + 1];
-                        span[offset + 1] = span[offset + 6];
-                        span[offset + 6] = temp;
-
-                        temp = span[offset + 2];
-                        span[offset + 2] = span[offset + 5];
-                        span[offset + 5] = temp;
-
-                        temp = span[offset + 3];
-                        span[offset + 3] = span[offset + 4];
-                        span[offset + 4] = temp;
-                    }
-                }
-
-                _hasCorrectEndianess = true;
             }
+            else if (itemSizeInBytes == 4)
+            {
+                for (var i = 0; i < _data.Length; i++)
+                {
+                    var offset = 4 * i;
+                        
+                    var temp = span[offset];
+                    span[offset] = span[offset + 3];
+                    span[offset + 3] = temp;
+
+                    temp = span[offset + 1];
+                    span[offset + 1] = span[offset + 2];
+                    span[offset + 2] = temp;
+                }
+            }
+            else if (itemSizeInBytes == 8)
+            {
+                for (var i = 0; i < _data.Length; i++)
+                {
+                    var offset = 8 * i;
+
+                    var temp = span[offset];
+                    span[offset] = span[offset + 7];
+                    span[offset + 7] = temp;
+
+                    temp = span[offset + 1];
+                    span[offset + 1] = span[offset + 6];
+                    span[offset + 6] = temp;
+
+                    temp = span[offset + 2];
+                    span[offset + 2] = span[offset + 5];
+                    span[offset + 5] = temp;
+
+                    temp = span[offset + 3];
+                    span[offset + 3] = span[offset + 4];
+                    span[offset + 4] = temp;
+                }
+            }
+
+            _hasCorrectEndianess = true;
         }
 
         protected internal Block(Descriptor desc) : base(desc)
